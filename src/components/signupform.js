3b6a8d3f5bee517cfee6a8a4a78d3d54ee682e7c
djc74 <davidcooper974@gmail.com>
2019-06-14 12:02:24 +0100

    adding signup form

---
+++ src/components/signupform.js
@@ -0,0 +1,63 @@
+import React from "react"
+import Link from "gatsby-link"
+
+import Layout from "../components/layout"
+
+import addToMailchimp from "gatsby-plugin-mailchimp"
+
+export default class SignupForm extends React.Component {
+  state = {
+    email: null,
+  }
+
+  _handleChange = e => {
+    console.log({
+      [`${e.target.name}`]: e.target.value,
+    })
+    this.setState({
+      [`${e.target.name}`]: e.target.value,
+    })
+  }
+
+  _handleSubmit = e => {
+    e.preventDefault()
+
+    console.log("submit", this.state)
+
+    addToMailchimp(this.state.email, this.state)
+      .then(({ msg, result }) => {
+        console.log("msg", `${result}: ${msg}`)
+
+        if (result !== "success") {
+          throw msg
+        }
+        alert(msg)
+      })
+      .catch(err => {
+        console.log("err", err)
+        alert(err)
+      })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h1>Hi people</h1>
+        <p>Submit the form below and check your browser console!</p>
+        <div>
+          <form onSubmit={this._handleSubmit}>
+            <input
+              type="email"
+              onChange={this._handleChange}
+              placeholder="email"
+              name="email"
+            />
+            <br />
+            <input type="submit" />
+          </form>
+        </div>
+        <Link to="/">Homepage</Link>
+      </Layout>
+    )
+  }
+}
