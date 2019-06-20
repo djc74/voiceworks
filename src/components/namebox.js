6cbd254b613210d0cb68182903cd023a9e78af61
djc74 <davidcooper974@gmail.com>
2019-06-20 23:49:29 +0100

    boxes styling

--- src/components/namebox.js
+++ src/components/namebox.js
@@ -1,25 +1,66 @@
 import React from "react"
 import styled from "styled-components"
 
+import chevron from "../icons/chevron.svg"
+import suze from "../images/suze-no-bg.png"
+
 const BoxesHolder = styled.section`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
+  margin-bottom: 5rem;
 `
 
 const BoxWrapper = styled.article`
   background: #f72f97;
   color: #fffafd;
-  height: 200px;
-  width: 45%;
+  padding: 2%;
+  /* width: 40%; */
+`
+
+const Name = styled.h1`
+  font-size: 5vw;
+  font-weight: 100;
+  margin-bottom: 1rem;
+  max-width: 60%;
+`
+
+const AboutWrapper = styled.div`
+  display: flex;
+`
+
+const AboutMe = styled.p`
+  font-weight: 700;
+  margin-left: 0.5rem;
+`
+
+const AboutChevron = styled.img`
+  height: 1rem;
+  width: 1rem;
+`
+
+const Headshot = styled.img`
+  height: 5rem;
+  position: relative;
+  top: 0;
+  right: 0;
 `
 
 const NameBoxes = () => (
   <BoxesHolder>
     <BoxWrapper>
-      <h1>Suze Cooper</h1>
+      <Name>Suze Cooper</Name>
+      <AboutWrapper>
+        <AboutChevron src={chevron} alt="chevron" />
+        <AboutMe>ABOUT ME</AboutMe>
+      </AboutWrapper>
+      <Headshot src={suze} alt="suze headshot" />
     </BoxWrapper>
     <BoxWrapper>
-      <h1>Peter Stewart</h1>
+      <Name>Peter Stewart</Name>
+      <AboutWrapper>
+        <AboutChevron src={chevron} alt="chevron" />
+        <AboutMe>ABOUT ME</AboutMe>
+      </AboutWrapper>
     </BoxWrapper>
   </BoxesHolder>
 )
