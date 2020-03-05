3b9e81a3a53012dc12ff6b94826145aaaf13c98d
djc74 <davidcooper974@gmail.com>
2020-03-05 11:05:01 +0000

    removing FlashBlurb from Index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import SignUpForm from "../components/signupform"
 import NameBoxes from "../components/namebox"
-import FlashBlurb from "../components/flashblurb"
 import Blurb from "../components/blurb"
 import FooterLinks from "../components/footerlinks"
 
@@ -12,7 +11,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Hello" />
     <NameBoxes />
-    <FlashBlurb />
     <Blurb />
     <SignUpForm />
     <FooterLinks />
